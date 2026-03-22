a5f5d654e7c7706ed4eb560c39d240118c8ed252
Ariel Weissman <arielbweissman@gmail.com>
2026-03-22 12:11:24 -0400

    Plan and Edit signup logic

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 app.secret_key = 'SecretKey'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
+
 
 with app.app_context():
     db.create_all()
@@ -37,6 +35,9 @@ def signup():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        if User.query.filter_by(username=username).first():
+            flash('Username already exists')
+            return redirect(url_for('signup'))
         new_user = User(username=username, password=password)
         db.session.add(new_user)
         db.session.commit()
